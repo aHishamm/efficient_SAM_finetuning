9cbbc7cd15e9770cba2affbc9b1f7c4a474eaa71
Abdulrahman Al Muaitah <a.almuaitah@gmail.com>
2024-02-07 16:36:02 +0400

    Updated code with DataLoader implemented

--- model_load.py
+++ model_load.py
@@ -1,10 +1,72 @@
 import torch
 from torch.optim import Adam
 from monai.losses import DiceFocalLoss
+from torch.utils.data import Dataset
+from datasets import load_dataset
 import transformers, datasets 
+from transformers import SamProcessor
 from tqdm import tqdm
 from statistics import mean 
+from torch.utils.data import DataLoader
 from torch.nn.functional import threshold, normalize
+
+#SAMDataset class for preprocessing input dataset for training 
+def get_bounding_box(ground_truth_map):
+  # get bounding box from mask
+  y_indices, x_indices = np.where(ground_truth_map > 0)
+  x_min, x_max = np.min(x_indices), np.max(x_indices)
+  y_min, y_max = np.min(y_indices), np.max(y_indices)
+  # add perturbation to bounding box coordinates
+  H, W = ground_truth_map.shape
+  x_min = max(0, x_min - np.random.randint(0, 20))
+  x_max = min(W, x_max + np.random.randint(0, 20))
+  y_min = max(0, y_min - np.random.randint(0, 20))
+  y_max = min(H, y_max + np.random.randint(0, 20))
+  bbox = [x_min, y_min, x_max, y_max]
+
+  return bbox
+
+
+class SAMDataset(Dataset):
+  def __init__(self, dataset, processor):
+    self.dataset = dataset
+    self.processor = processor
+
+  def __len__(self):
+    return len(self.dataset)
+
+  def __getitem__(self, idx):
+    item = self.dataset[idx]
+    image = item["image"]
+    ground_truth_mask = np.array(item["label"])
+
+    # get bounding box prompt
+    prompt = get_bounding_box(ground_truth_mask)
+
+    # prepare image and prompt for the model
+    inputs = self.processor(image, input_boxes=[[prompt]], return_tensors="pt")
+
+    # remove batch dimension which the processor adds by default
+    inputs = {k:v.squeeze(0) for k,v in inputs.items()}
+
+    # add ground truth segmentation
+    inputs["ground_truth_mask"] = ground_truth_mask
+
+    return inputs
+
+
+#Loading the dataset for training 
+ds = load_dataset('ahishamm/isic_masks',split='train') 
+
+
+
+#Import SAMProcessor processor information 
+processor = SamProcessor.from_pretrained('facebook/sam-vit-base')
+print(processor)
+
+train_dataset = SAMDataset(dataset=ds,processor=processor)
+train_dataloader = DataLoader(train_dataset,batch_size=4,shuffle=True)
+
 #Loading the torchscript weights into a model 
 loaded_model = torch.jit.load("/Users/ahishamm/Documents/projects/efficientSAM/EfficientSAM/torchscripted_model/efficient_sam_vits_torchscript.pt")
 loaded_model.eval() 
